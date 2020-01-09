d56892cb783b08ab4aba5725400b467920f55818
Ray Gervais <raygervais@hotmail.ca>
2020-01-08 21:24:12 -0500

    Adds: RSS Feed Generation, Sitemap and Robots.txt (#14)
    * 13: adds rss generation plugin
    
    * 13: adds feed generation plugin
    
    * 13: adds sitemap and robots.txt

--- gridsome.config.js
+++ gridsome.config.js
@@ -6,11 +6,12 @@
 
 module.exports = {
   siteName: "Ray Gervais",
+  siteUrl: "https://raygervais.dev",
   siteDescription:
     "Topics of Programming, Techno-babble, Music, and Life through the eyes of a Canadian Software Developer.",
 
   templates: {
-    Post: "/:title",
+    Post: "/article/:title",
     Tag: "/tag/:id"
   },
 
@@ -29,13 +30,72 @@ module.exports = {
           }
         }
       }
+    },
+
+    {
+      use: "gridsome-plugin-feed",
+      options: {
+        // Required: array of `GraphQL` type names you wish to include
+        contentTypes: ["Post"],
+        // Optional: any properties you wish to set for `Feed()` constructor
+        // See https://www.npmjs.com/package/feed#example for available properties
+        feedOptions: {
+          title: "Ray Gervais",
+          description:
+            "Topics of Programming, Techno-babble, Music, and Life through the eyes of a Canadian Software Developer."
+        },
+        // === All options after this point show their default values ===
+        // Optional; opt into which feeds you wish to generate, and set their output path
+        rss: {
+          enabled: true,
+          output: "/feed.xml"
+        },
+        atom: {
+          enabled: false,
+          output: "/feed.atom"
+        },
+        json: {
+          enabled: true,
+          output: "/feed.json"
+        },
+        // Optional: the maximum number of items to include in your feed
+        maxItems: 25,
+        // Optional: an array of properties passed to `Feed.addItem()` that will be parsed for
+        // URLs in HTML (ensures that URLs are full `http` URLs rather than site-relative).
+        // To disable this functionality, set to `null`.
+        htmlFields: ["description", "content"],
+        // Optional: if you wish to enforce trailing slashes for site URLs
+        enforceTrailingSlashes: false,
+        // Optional: a method that accepts a node and returns true (include) or false (exclude)
+        // Example: only past-dated nodes: `filterNodes: (node) => node.date <= new Date()`
+        filterNodes: node => true,
+        // Optional: a method that accepts a node and returns an object for `Feed.addItem()`
+        // See https://www.npmjs.com/package/feed#example for available properties
+        // NOTE: `date` field MUST be a Javascript `Date` object
+        nodeToFeedItem: node => ({
+          title: node.title,
+          date: node.date || node.fields.date,
+          content: node.content
+        })
+      }
+    },
+    {
+      use: "@gridsome/plugin-sitemap",
+      options: {
+        cacheTime: 600000, // default
+        exclude: ["/exclude-me"],
+        config: {
+          "/article/*": {
+            changefreq: "weekly",
+            priority: 0.5
+          },
+          "/about": {
+            changefreq: "monthly",
+            priority: 0.7
+          }
+        }
+      }
     }
-    // {
-    //   use: "@gridsome/plugin-google-analytics",
-    //   options: {
-    //     id: process.env.Google_Analytics_ID
-    //   }
-    // }
   ],
 
   transformers: {
