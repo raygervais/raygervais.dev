b014bd8a687f82ca90f293ffdb915730798b3631
RayGervais <raygervais@hotmail.ca>
2020-01-19 22:05:36 -0500

    fixes: tag a11y and formatting

--- src/components/PostTags.vue
+++ src/components/PostTags.vue
@@ -1,30 +1,55 @@
 <template>
-   <div class="post-tags">
-   		<g-link class="post-tags__link" v-for="tag in post.tags" :key="tag.id" :to="tag.path">
-   			<span>#</span> {{ tag.title }}
-   		</g-link>
+  <div>
+    <h2>Tags</h2>
+    <div class="post-tags">
+      <br />
+      <g-link
+        class="post-tags__link"
+        v-for="tag in post.tags"
+        :key="tag.id"
+        :to="tag.path"
+      >
+        {{ tag.title }}
+      </g-link>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-  props: ['post']
-}
+  props: ["post"]
+};
 </script>
 
 <style lang="scss">
 .post-tags {
   margin: 1em 0 0;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  flex: 1;
 
   &__link {
-  	margin-right: .7em;
-  	font-size: .8em;
-  	color: currentColor;
-  	text-decoration: none;
-  	background-color: var(--bg-color);
-  	color: currentColor!important; //Todo: remove important;
-  	padding: .5em;
-  	border-radius: var(--radius);
+    margin-right: 0.7em;
+    font-size: 0.75em;
+    color: currentColor;
+    text-decoration: none;
+    background-color: var(--bg-color);
+    color: currentColor !important; //Todo: remove important;
+    padding: 0.5em;
+    border-radius: var(--radius);
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+    // min-width: 100px;
+	flex-grow: 1;
+    width: auto;
+    height: 75px;
   }
 }
 </style>
